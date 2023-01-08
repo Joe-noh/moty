2b47c9ce83dd7ecd8db202d5723dde8fb0d6ac46
Joe-noh <goflb.jh@gmail.com>
2023-01-09 05:14:21 +0900

    redirect to [year]/movies if logged in

--- src/routes/[year=year]/+page.ts
+++ src/routes/[year=year]/+page.ts
@@ -1,8 +1,15 @@
+import { redirect } from '@sveltejs/kit'
 import type { PageLoad } from './$types'
 
-export const load: PageLoad = ({ params }) => {
-  return {
-    year: parseInt(params.year),
-    movies: [],
+export const load: PageLoad = async ({ params, parent }) => {
+  const { user } = await parent()
+
+  if (user) {
+    throw redirect(302, `/${params.year}/movies`)
+  } else {
+    return {
+      year: parseInt(params.year),
+      movies: [],
+    }
   }
 }
