aac768bb7c61c5ba6800682d7984944e5690d8e5
Joe-noh <goflb.jh@gmail.com>
2023-01-08 23:05:05 +0900

    create user and set cookie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,14 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import { initializeFirebase, login } from '$lib/firebase'
-  import { trpc } from '$lib/trpc/client'
 
   onMount(async () => initializeFirebase())
-
-  async function createUser() {
-    const hello = await trpc.greeting.query()
-
-    console.log(hello)
-  }
 </script>
 
 <h1>Movie of the Year</h1>
 
 <button on:click={() => login()}>LOGIN</button>
 
-<button on:click={() => createUser()}>CREATE USER</button>
+<form action="/users" method="post">
+  <button type="submit">CREATE USER</button>
+</form>
---
+++ src/routes/users/+page.server.ts
@@ -0,0 +1,29 @@
+import { redirect } from '@sveltejs/kit'
+import { nanoid } from 'nanoid'
+import * as jose from 'jose'
+import { prisma } from '$lib/prisma'
+import { JWT_SECRET } from '$env/static/private'
+import type { Actions } from './$types'
+
+function cookieExpires(): Date {
+  const date = new Date()
+  date.setDate(date.getDate() + 14)
+
+  return date
+}
+
+export const actions: Actions = {
+  default: async ({ cookies }) => {
+    const user = await prisma.user.create({ data: { uuid: nanoid(10) } })
+    const jwt = await new jose.SignJWT({ uuid: user.uuid })
+      .setProtectedHeader({ alg: 'HS512' })
+      .setIssuedAt()
+      .setIssuer('MOTY')
+      .setExpirationTime('2weeks')
+      .sign(new TextEncoder().encode(JWT_SECRET))
+
+    cookies.set('token', jwt, { expires: cookieExpires() })
+
+    throw redirect(302, '/2023')
+  },
+}
