ddf1b4a63fd761b8170eae92746e2528c05f10c3
Joe-noh <goflb.jh@gmail.com>
2023-01-09 17:25:49 +0900

    skelton pages

---
+++ src/routes/[year=year]/[uuid]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { PageData } from './$types'
+
+  export let data: PageData
+</script>
+
+<h1>Search</h1>
+
+<ol>
+  {#each data.movies as movie}
+    <li>{movie}</li>
+  {/each}
+</ol>
