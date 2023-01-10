dd47993fa71db69496114b66c2e8e85d622d285b
Joe-noh <goflb.jh@gmail.com>
2023-01-10 23:10:23 +0900

    get movies from tmdb

--- src/routes/[year=year]/search/+page.svelte
+++ src/routes/[year=year]/search/+page.svelte
@@ -1,1 +1,19 @@
+<script lang="ts">
+  import type { ActionData, PageData } from './$types'
+
+  export let data: PageData
+  export let form: ActionData
+
+  $: movies = form?.movies || []
+</script>
+
 <h1>Search</h1>
+
+<form method="post" action={`/${data.year}/search`}>
+  <input type="text" name="query" />
+  <button type="submit">SEARCH</button>
+</form>
+
+{#each movies as movie}
+  <li>{movie.title}</li>
+{/each}
