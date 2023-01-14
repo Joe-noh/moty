2824694457bfec3f46805fc7528fe6853a771959
Joe-noh <goflb.jh@gmail.com>
2023-01-15 02:56:01 +0900

    install vitest-environment-vprisma

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,14 @@ const config: UserConfig = {
   plugins: [sveltekit()],
   test: {
     globals: true,
+    environment: 'vprisma',
     include: ['src/**/*.{test,spec}.{js,ts}'],
-    setupFiles: ['test/setup.ts'],
+    setupFiles: ['vitest-environment-vprisma/setup', 'test/setup.ts'],
+    environmentOptions: {
+      vprisma: {
+        databaseUrl: 'postgres://moty:password@localhost:5433/moty_test',
+      },
+    },
   },
 }
 
